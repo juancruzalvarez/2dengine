5cae896791b4af335316df50af53b8c34109c35d
Juan Cruz Alvarez <juancruzalvarezgonzalez@gmail.com>
2023-02-26 16:02:47 -0300

    fewa

--- app.cc
+++ app.cc
@@ -26,11 +26,11 @@ App::App() {
 	uint8_t* bitmap_img;
 	std::map<uint8_t, graphics::CharInfo> charmap;
 	glm::ivec2 bitmap_img_size;
-	graphics::LoadFont("res/fonts/arialbd.ttf", 32, &bitmap_img, &bitmap_img_size , &charmap);
+	graphics::LoadFont("res/fonts/CascadiaCode.ttf", 32, &bitmap_img, &bitmap_img_size , &charmap);
 	std::cout << "charmap_size:" << charmap.size() << "\n";
 	std::cout << "A:" << charmap['a'].size.x << " " << charmap['a'].size.y << "\n";
 	glfwInit();
-	window_ = new Window{"hello", 300, 300};
+	window_ = new Window{"hello", 800, 800};
 	window_->set_key_callback( [&](int glfw_key_code, int scancode, Input::Action action, int modifiers) {
 		key_callback(glfw_key_code, scancode, action, modifiers);
 	});
@@ -45,7 +45,7 @@ App::App() {
 	renderer_ = new graphics::Renderer();
 	int width, height, nrChannels;
 	stbi_set_flip_vertically_on_load(true); 
-	unsigned char* data = stbi_load("res/sprites/test.png", &width, &height, &nrChannels, 4);
+	uint8_t* data = stbi_load("res/sprites/test.png", &width, &height, &nrChannels, 4);
 	if (!data)
 	{
 		std::cout << "Failed to load texture" << std::endl;
@@ -55,14 +55,14 @@ App::App() {
 		GL_RGBA8,
 		GL_LINEAR,
 		GL_REPEAT,
-		width, height
+		bitmap_img_size.x, bitmap_img_size.y
 	};
 
 	 t = graphics::Texture(
 		info,
-		GL_RGBA,
+		GL_RED,
 		GL_UNSIGNED_BYTE,
-		(void*)data
+		(void*)bitmap_img
 	);
 }
 void App::Run() {
@@ -71,8 +71,8 @@ void App::Run() {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		std::vector<float> vec = { 1, 1, 1, 0, 0,0, 0, 1 };
-		renderer_->Render(graphics::Quad{ {0.0,0.0},{0.8,0.8},vec }, t.texture_id);
-		renderer_->Render(graphics::Quad{ {0.3,0.5},{0.4,0.6},vec }, t.texture_id);
+		renderer_->Render(graphics::Quad{ {0.0,0.0},{2,2},vec }, t.texture_id);
+		//renderer_->Render(graphics::Quad{ {0.3,0.5},{0.4,0.6},vec }, t.texture_id);
 
 		renderer_->Flush();
 		if (window_->has_resized())
--- font_loading.cc
+++ font_loading.cc
@@ -42,19 +42,17 @@ void LoadFont(
 			uint8_t current_char = r * 16 + c;
 			error = FT_Load_Char(face, current_char, FT_LOAD_RENDER);
 			if (error) continue;
-			std::cout << "2\n";
 			charmap->insert({
 				current_char,
 				{
 					{x, y}, 
 					{x+face->glyph->bitmap.width, y+face->glyph->bitmap.rows},
-					{face->glyph->metrics.width, face->glyph->metrics.height},
+					{face->glyph->bitmap.width, face->glyph->bitmap.rows},
 					{face->glyph->metrics.horiBearingX, face->glyph->metrics.horiBearingY},
 					face->glyph->metrics.horiAdvance,
 
 				}
 			});
-			x += face->glyph->bitmap.width;
 			if (face->glyph->bitmap.rows > current_max_height) {
 				current_max_height = face->glyph->bitmap.rows;
 			}
@@ -62,21 +60,14 @@ void LoadFont(
 				// bitmap buffer stores pixels in decreasing vertical position. First bytes in the buffer correspond to
 				// the topmost pixels in the bitmap.
 				for (int i = 0; i < (*charmap)[current_char].size.y; i++) {
-					std::cout << "3\n";
 
 					glm::ivec2 tex_pos = glm::ivec2(x, y + ((*charmap)[current_char].size.y - i));
-					std::cout << x << " " << (*charmap)[current_char].size.y << "\n";
-					std::cout << tex_pos.y * tex_size + tex_pos.x << "\n";
-					auto f = &((*bitmap_img)[tex_pos.y * tex_size + tex_pos.x]);
-					std::cout << "help" << f << "help\n";
-					
 
 					memcpy(
-						f,
+						&((*bitmap_img)[tex_pos.y * tex_size + tex_pos.x]) ,
 						&(face->glyph->bitmap.buffer[i * face->glyph->bitmap.pitch]),
 						(*charmap)[current_char].size.x
 					);
-					std::cout << "5\n";
 				}
 			}
 			else {
@@ -86,14 +77,14 @@ void LoadFont(
 
 					glm::ivec2 tex_pos = glm::ivec2(x, y + i);
 					memcpy(
-						&((*bitmap_img)[tex_pos.y * tex_size + tex_size]),
+						&((*bitmap_img)[tex_pos.y * tex_size + tex_pos.x]),
 						&(face->glyph->bitmap.buffer[i * face->glyph->bitmap.pitch]),
 						(*charmap)[current_char].size.x
 					);
 
 				}
 			}
-			
+			x += face->glyph->bitmap.width;
 		}
 		y += current_max_height;
 		if (x > current_max_width)
